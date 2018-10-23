b4d0f8f8cb9545fdf9c55b9733f26db25c684bc3
moisesabraao <moisesdirt_bmx@hotmail.com>
2018-10-22 21:33:17 -0300

    Adicionando o setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Pyliticamemes',
+    version='0.1',
+    url='https://github.com/MoisesAbraao/PyliticaMemes',
+    license='MIT',
+    author='Moises Abraao',
+    author_email='moisesabraaoso@gmail.com',
+    description='zoeira com os memes da politica brasileira',
+    long_description='',
+    packages=['pyliticamemes'],
+    install_requires=['pygame>=1.9.4'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Libraries :: Python Modules'
+        ]
+    )
